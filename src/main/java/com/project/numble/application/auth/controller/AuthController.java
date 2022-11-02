1640f6d969a27bfa0e6fa5fbacf537b51eab44e9
apptie <appti@naver.com>
2022-11-03 00:14:02 +0900

    refactor: AuthController @Validated에서 @Valid로 변경

--- src/main/java/com/project/numble/application/auth/controller/AuthController.java
+++ src/main/java/com/project/numble/application/auth/controller/AuthController.java
@@ -2,15 +2,10 @@ package com.project.numble.application.auth.controller;
 
 import com.project.numble.application.auth.dto.request.SignUpRequest;
 import com.project.numble.application.user.service.UserService;
-import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +19,7 @@ public class AuthController {
     private final UserService userService;
 
     @PostMapping("/sign-up")
-    public ResponseEntity<Void> signUp(@Validated @RequestBody SignUpRequest request) {
+    public ResponseEntity<Void> signUp(@Valid @RequestBody SignUpRequest request) {
         userService.signUp(request);
 
         return new ResponseEntity<>(HttpStatus.CREATED);
