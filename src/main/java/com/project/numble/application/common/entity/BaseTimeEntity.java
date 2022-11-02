7e97e17d910666f85528d3220afeb86032cd68ac
apptie <appti@naver.com>
2022-11-02 21:16:51 +0900

    feat: 시간 관련 공통 Entity 추가

---
+++ src/main/java/com/project/numble/application/common/entity/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.project.numble.application.common.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "create_by", updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "modify_by")
+    private LocalDateTime lastModifiedDate;
+}
