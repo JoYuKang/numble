5c1e69884aed7ce9a8da7c55a7622f564a5766be
apptie <appti@naver.com>
2022-11-02 21:22:17 +0900

    refactor: Address Entity 테이블 이름 변경

--- src/main/java/com/project/numble/application/user/domain/Address.java
+++ src/main/java/com/project/numble/application/user/domain/Address.java
@@ -8,12 +8,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Table(name = "tb_address")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address {
 
