a994a900c037b2195bb2f14e3a5fe59631d138b7
apptie <appti@naver.com>
2022-11-04 11:25:38 +0900

    feat: 로그인 기능 AuthService.signUp 추가

--- src/main/java/com/project/numble/application/auth/controller/AuthController.java
+++ src/main/java/com/project/numble/application/auth/controller/AuthController.java
@@ -1,11 +1,17 @@
 package com.project.numble.application.auth.controller;
 
+import com.project.numble.application.auth.dto.request.SignInRequest;
 import com.project.numble.application.auth.dto.request.SignUpRequest;
+import com.project.numble.application.auth.service.AuthService;
 import com.project.numble.application.user.service.UserService;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +23,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UserService userService;
+    private final AuthService authService;
 
     @PostMapping("/sign-up")
     public ResponseEntity<Void> signUp(@Valid @RequestBody SignUpRequest request) {
         userService.signUp(request);
 
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<Void> signIn(@Validated @RequestBody SignInRequest request,
+        HttpSession session) {
+        authService.signIn(request);
+
+        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+            SecurityContextHolder.getContext());
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
