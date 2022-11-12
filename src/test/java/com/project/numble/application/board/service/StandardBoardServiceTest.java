06ea04b0f417294bc747e2f02d3ffdbd5074aba4
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2022-11-12 15:27:04 +0900

    test : board 로직 변경 작업을 위한 test 삭제

--- src/test/java/com/project/numble/application/board/service/StandardBoardServiceTest.java
+++ src/test/java/com/project/numble/application/board/service/StandardBoardServiceTest.java
@@ -7,6 +7,7 @@ import com.project.numble.application.board.dto.response.GetAllBoardResponse;
 import com.project.numble.application.board.dto.response.GetBoardResponse;
 import com.project.numble.application.board.repository.BoardRepository;
 import com.project.numble.application.board.service.exception.BoardNotExistsException;
+import com.project.numble.application.helper.factory.entity.UserFactory;
 import com.project.numble.application.user.domain.User;
 import com.project.numble.application.user.repository.UserRepository;
 import com.project.numble.application.user.service.StandardUserService;
@@ -42,134 +43,40 @@ class StandardBoardServiceTest {
     @Test
     @DisplayName("board 저장 테스트")
     void save() {
-        User user = getUser("use@test.com", "test!");
 
-        AddBoardRequest boardRequest = getAddBoardRequest(user, "testContent");
-
-        Long saveId = boardService.addBoard(boardRequest, user.getId());
-
-        Optional<User> userByEmail = userRepository.findByEmail("use@test.com");
-
-        User findUser = userByEmail.get();
-
-        GetBoardResponse getBoardRequest = boardService.getBoard(saveId);
-
-        Board board = getBoardRequest.toEntity();
-
-
-        log.info("user email :" + findUser.getEmail());
-        log.info("user nickname:" + findUser.getNickname());
-        log.info("boardContent : " + board.getContent());
-        log.info("board null check : " + board.getUser());
-
-        Assertions.assertThat(board.getContent()).isEqualTo("testContent");
+        User user = UserFactory.createStaticUser();
+//        userRepository.save(user);
+//        userRepository.save(user);
+//        AddBoardRequest request = AddBoardRequest.builder()
+//                .content("test content")
+//                .address(user.getAddress())
+//                .user(user)
+//                .build();
+//        Long boardId = boardService.addBoard(request, user.getId());
+//
+//
+//        Board board = boardRepository.findById(boardId).orElseThrow(() -> new BoardNotExistsException());
+//        log.info("content " + board.getContent());
+//        log.info("nickname " + board.getUser().getNickname());
+//        log.info("address " + board.getAddress().getAddressName());
+//        log.info("region1 " + board.getAddress().getRegionDepth1());
+//        log.info("region2 " + board.getAddress().getRegionDepth2());
     }
 
     @Test
     @DisplayName("수정 테스트")
     void update(){
-        User user = User.createNormalUser("use@test.com", "1234", "test!");
-
-        AddBoardRequest boardRequest = getAddBoardRequest(user, "before test content");
-
-        userRepository.save(user);
-        Long saveId = boardService.addBoard(boardRequest, user.getId());
-
-        String updateText = "after test content";
-
-        GetBoardResponse getBeforeBoardRequest = boardService.getBoard(saveId);
-        Optional<Board> optionalBoard1 = boardRepository.findAllById(saveId);
-        Board beforeBoard = optionalBoard1.orElseThrow(() -> new BoardNotExistsException());
-
-        log.info("beforeBoard.getUser().getNickname() : " + beforeBoard.getUser().getNickname());
-        log.info("수정 전 content : " + beforeBoard.getContent());
-
-        ModBoardRequest modBoardRequest = ModBoardRequest.builder()
-                .content(updateText)
-                .build();
-
-        Long afterId = boardService.updateBoard(saveId, user.getId(), modBoardRequest);
-
-        Optional<Board> optionalBoard2 = boardRepository.findAllById(afterId);
-        Board afterBoard = optionalBoard2.orElseThrow(() -> new BoardNotExistsException());
-
-        log.info("afterBoard.getUser().getNickname() : " + afterBoard.getUser().getNickname());
-        log.info("수정 후 content : " + afterBoard.getContent());
-
-
-        Assertions.assertThat(beforeBoard.getUser().getNickname()).isEqualTo(afterBoard.getUser().getNickname());
     }
 
     @Test
     @DisplayName("Board 전체 조회")
     void searchAll(){
 
-        User user1 = User.createNormalUser("use1@test.com", "1234", "test1!!");
-
-        AddBoardRequest boardRequest1 = getAddBoardRequest(user1, "test content 111111");
-
-        userRepository.save(user1);
-        Long saveId1 = boardService.addBoard(boardRequest1, user1.getId());
-
-        User user2 = User.createNormalUser("use2@test.com", "1234", "test2@@");
-
-        AddBoardRequest boardRequest2 = getAddBoardRequest(user2, "test content 222222");
-
-        userRepository.save(user2);
-        Long saveId2 = boardService.addBoard(boardRequest2, user1.getId());
-
-        List<GetAllBoardResponse> boardList = boardService.getBoardList();
-
-        for (GetAllBoardResponse board: boardList) {
-            log.info(">>>> "+String.valueOf(board.getNickname()));
-            log.info(">>>> "+String.valueOf(board.getContent()));
-        }
-
-        Assertions.assertThat(boardList.size()).isEqualTo(2);
-
     }
 
     @Test
     @DisplayName("user가 쓴 board 조회")
     void searchAllByUser(){
-        User user1 = getUser("use1@test.com", "test1!!");
-
-        AddBoardRequest boardRequest1 = getAddBoardRequest(user1, "same user 111111");
-        boardService.addBoard(boardRequest1, user1.getId());
-
-        AddBoardRequest boardRequest2 = getAddBoardRequest(user1, "same user 222222");
-        boardService.addBoard(boardRequest2, user1.getId());
-
-        User user2 = getUser("use2@test.com", "test2@@");
-
-        AddBoardRequest boardRequest3 = getAddBoardRequest(user2, "other user");
-
-        userRepository.save(user2);
-        boardService.addBoard(boardRequest3, user2.getId());
-
-        List<Board> all = boardRepository.findAll();
-
-        log.info("================ all ================");
-        for (Board board : all) {
-            log.info("board.getContent() " + board.getContent());
-        }
-
-        List<GetBoardResponse> boardUser = boardService.getBoardUser(user1.getId());
-        log.info("================ user1 ================");
-        for (GetBoardResponse getBoardResponse : boardUser) {
-            log.info("boardUser getNickname" + getBoardResponse.getNickname());
-            log.info("boardUser getContent" + getBoardResponse.getContent());
-        }
-
-        log.info("==================================");
-        log.info("board size = " + boardUser.size());
-        List<Board> boards = user1.getBoards();
-        for (Board board1 : boards) {
-            log.info(">>> board user nickname" + board1.getUser().getNickname());
-            log.info(">>> board content" + board1.getContent());
-        }
-
-        Assertions.assertThat(boardUser.size()).isEqualTo(2);
 
     }
 
@@ -177,61 +84,7 @@ class StandardBoardServiceTest {
     @Test
     @DisplayName("board 삭제 후 조회 test")
     void del(){
-        User user1 = getUser("use1@test.com", "test1!!");
-
-        AddBoardRequest boardRequest1 = getAddBoardRequest(user1, "same user 111111");
-        Long DelId = boardService.addBoard(boardRequest1, user1.getId());
-
-        AddBoardRequest boardRequest2 = getAddBoardRequest(user1, "same user 222222");
-        boardService.addBoard(boardRequest2, user1.getId());
-
-        List<GetBoardResponse> boardUser = boardService.getBoardUser(user1.getId());
-
-        log.info("============== board list ==============");
-        for (GetBoardResponse getBoardResponse : boardUser) {
-            log.info("getBoardResponse getNickname " + getBoardResponse.getNickname());
-            log.info("getBoardResponse getContent " + getBoardResponse.getContent());
-        }
-
-        log.info("========= before user board list =========");
-        List<Board> beforeUserBoards = user1.getBoards();
-        for (Board board : beforeUserBoards) {
-            log.info("board.getContent() " + board.getContent());
-        }
-
-        Assertions.assertThat(beforeUserBoards.size()).isEqualTo(2);
-
-        boardService.delete(DelId);
-
-        List<Board> allBoard = boardRepository.findAll();
-        log.info("========= after board list =========");
-        for (Board board : allBoard) {
-            log.info("board.getNickname() " + board.getUser().getNickname());
-            log.info("board.getContent() " + board.getContent());
-        }
-        log.info("========= after user board list =========");
-        List<Board> afterUserBoards = user1.getBoards();
-        for (Board board : afterUserBoards) {
-            log.info("board.getContent() " + board.getContent());
-        }
-
-
-        Assertions.assertThat(afterUserBoards.size()).isEqualTo(1);
 
     }
 
-    // AddBoardRequest 생성
-    private static AddBoardRequest getAddBoardRequest(User user1, String content) {
-        AddBoardRequest boardRequest1 = AddBoardRequest.builder()
-                .user(user1)
-                .content(content).build();
-        return boardRequest1;
-    }
-
-    // user 생성
-    private User getUser(String email, String nickname) {
-        User user = User.createNormalUser(email, "1234", nickname);
-        userRepository.save(user);
-        return user;
-    }
 }
