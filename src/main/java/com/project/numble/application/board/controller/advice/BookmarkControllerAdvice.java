698b4dc02a63779c1563531132b71844081ee410
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2022-11-18 19:19:19 +0900

    feat : bookmark 예외 추가

---
+++ src/main/java/com/project/numble/application/board/controller/advice/BookmarkControllerAdvice.java
@@ -0,0 +1,33 @@
+package com.project.numble.application.board.controller.advice;
+
+import com.project.numble.application.board.controller.BookmarkController;
+import com.project.numble.application.board.service.exception.AlreadyBookmarkBoardException;
+import com.project.numble.application.board.service.exception.BookmarkNotExistsException;
+import com.project.numble.application.common.advice.ControllerAdviceUtils;
+import com.project.numble.application.common.advice.ExceptionType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RestControllerAdvice(assignableTypes = {BookmarkController.class})
+public class BookmarkControllerAdvice {
+
+    private final ControllerAdviceUtils utils;
+
+    @ExceptionHandler(AlreadyBookmarkBoardException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    Map<String, String> alreadyBookmarkBoardExceptionHandler() {
+        return utils.getFailureResponse(ExceptionType.ALREADY_BOOKMARK_BOARD_EXCEPTION);
+    }
+
+    @ExceptionHandler(BookmarkNotExistsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    Map<String, String> bookmarkNotExistsExceptionHandler() {
+        return utils.getFailureResponse(ExceptionType.BOOKMARK_NOT_EXIST_EXCEPTION);
+    }
+}
