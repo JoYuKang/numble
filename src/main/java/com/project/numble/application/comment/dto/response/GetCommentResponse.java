7fd20a58bc2bd6d50bf94c8bfb65dde2a9a738b0
apptie <appti@naver.com>
2022-11-20 22:42:37 +0900

    refactor: Comment response 생성자 추가

--- src/main/java/com/project/numble/application/comment/dto/response/GetCommentResponse.java
+++ src/main/java/com/project/numble/application/comment/dto/response/GetCommentResponse.java
@@ -10,9 +10,11 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class GetCommentResponse {
 
