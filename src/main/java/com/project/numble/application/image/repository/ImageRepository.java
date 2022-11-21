cb2effd39c148480381c93fbece07a2ecd4ff6fe
apptie <57691173+apptie@users.noreply.github.com>
2022-11-21 22:28:54 +0900

    refactor: Board 기반으로 Image Entity를 조회하는 기능 추가 (#42)

--- src/main/java/com/project/numble/application/image/repository/ImageRepository.java
+++ src/main/java/com/project/numble/application/image/repository/ImageRepository.java
@@ -1,8 +1,11 @@
 package com.project.numble.application.image.repository;
 
+import com.project.numble.application.board.domain.Board;
 import com.project.numble.application.image.domain.Image;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
+    List<Image> findAllByBoard(Board board);
 }
