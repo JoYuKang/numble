09debf432b36d19f3515af738d8c08d3ce69eabf
apptie <appti@naver.com>
2022-11-29 19:44:07 +0900

    refactor: Cors Session Header 설정 변경

--- src/main/java/com/project/numble/config/SessionConfig.java
+++ src/main/java/com/project/numble/config/SessionConfig.java
@@ -2,10 +2,12 @@ package com.project.numble.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
 import org.springframework.session.web.http.HttpSessionIdResolver;
 
 @Configuration
+@EnableSpringHttpSession
 public class SessionConfig {
 
     @Bean
